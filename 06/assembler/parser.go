ad036f4baf614e7b3879ddfa240bd0821173a7b5
bbbbruno <bbbbruno69@gmail.com>
2022-01-26 17:57:55 +0900

    parserをシンボルテーブルを受け取るように修正

--- 06/assembler/parser.go
+++ 06/assembler/parser.go
@@ -12,18 +12,22 @@ func NewParser() Parser {
 	return Parser{}
 }
 
-func (p Parser) Parse(input io.Reader) (cmds []*Command) {
+// 入力から一行ずつ解析する作業を二度行い、コマンドの配列とシンボルテーブルを作成する。
+// 1回目の解析ではLコマンドのみ解析し、次の行のアドレスをシンボルテーブルに格納する。
+// 2回目の解析ではAコマンドとCコマンドのみを解析し、ユーザー定義シンボルもシンボルテーブルに格納する。
+func (p Parser) Parse(input io.Reader) (cmds []*Command, st *SymbolTable) {
 	lines := parseInput(input)
 
-	for _, l := range lines {
-		cmds = append(cmds, parseLine(l))
-	}
+	st = parseSymbols(lines)
+	cmds, st = parseLines(lines, st)
 
-	return cmds
+	return cmds, st
 }
 
+// 入力から一行ずつ読み取り、空白行とコメントアウトを取り除く。
 func parseInput(input io.Reader) (lines []string) {
 	s := bufio.NewScanner(input)
+
 	for s.Scan() {
 		line := s.Text()
 		line = strings.ReplaceAll(line, " ", "")         // 空白除去
@@ -35,27 +39,63 @@ func parseInput(input io.Reader) (lines []string) {
 		}
 		lines = append(lines, line)
 	}
+
 	return lines
 }
 
-func parseLine(line string) *Command {
-	cmd := new(Command)
+// Lコマンドのみ解析し、ユーザー定義シンボルをシンボルテーブルに格納する。
+func parseSymbols(lines []string) *SymbolTable {
+	st := NewSymbolTable()
+	var count addr
 
-	if line[0] == '@' {
-		cmd.Type = A_COMMAND
-		cmd.Symbol = line[1:]
-		return cmd
+	for _, l := range lines {
+		ok, sym := IsLCommand(l)
+		if !ok {
+			count += 1
+			continue
+		}
+
+		if _, ok := st.Symbols[sym]; ok {
+			continue
+		}
+
+		st.Symbols[sym] = count
 	}
 
-	cmd.Type = C_COMMAND
-	cmd.Comp = line
-	if ss := strings.Split(line, "="); len(ss) == 2 {
-		cmd.Dest = ss[0]
-		cmd.Comp = strings.ReplaceAll(cmd.Comp, cmd.Dest+"=", "")
+	return st
+}
+
+// AコマンドとCコマンドのみ解析し、適時ユーザー定義シンボルをシンボルテーブルに格納する。
+func parseLines(lines []string, st *SymbolTable) ([]*Command, *SymbolTable) {
+	var cmds []*Command
+	for _, l := range lines {
+		if ok, _ := IsLCommand(l); ok {
+			continue
+		}
+
+		cmd := new(Command)
+		if ok, sym := IsACommand(l); ok {
+			cmd.Type = A_COMMAND
+			cmd.Symbol = sym
+			if _, ok := st.Addr(cmd.Symbol); !ok {
+				st.AddSymbol(cmd.Symbol)
+			}
+			cmds = append(cmds, cmd)
+			continue
+		}
+
+		cmd.Type = C_COMMAND
+		cmd.Comp = l
+		if ss := strings.Split(l, "="); len(ss) == 2 {
+			cmd.Dest = ss[0]
+			cmd.Comp = strings.ReplaceAll(cmd.Comp, cmd.Dest+"=", "")
+		}
+		if ss := strings.Split(l, ";"); len(ss) == 2 {
+			cmd.Jump = ss[1]
+			cmd.Comp = strings.ReplaceAll(cmd.Comp, ";"+cmd.Jump, "")
+		}
+		cmds = append(cmds, cmd)
 	}
-	if ss := strings.Split(line, ";"); len(ss) == 2 {
-		cmd.Jump = ss[1]
-		cmd.Comp = strings.ReplaceAll(cmd.Comp, ";"+cmd.Jump, "")
-	}
-	return cmd
+
+	return cmds, st
 }
