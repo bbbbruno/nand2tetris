240a537f5573ea32545fd649ffe017bcc3aa5785
bbbbruno <bbbbruno69@gmail.com>
2022-01-29 17:31:26 +0900

    fixturesにStackTestを追加

--- 07/VMConverter/main_test.go
+++ 07/VMConverter/main_test.go
@@ -8,10 +8,24 @@ import (
 )
 
 func TestConvertSimpleAdd(t *testing.T) {
-	vmFile, _ := os.ReadFile(filepath.Join("tests", "SimpleAdd", "SimpleAdd.vm"))
+	vmFile, _ := os.ReadFile(filepath.Join("fixtures", "SimpleAdd", "SimpleAdd.vm"))
 	in := bytes.NewBuffer(vmFile)
 	got := bytes.NewBuffer([]byte{})
-	asmFile, _ := os.ReadFile(filepath.Join("tests", "SimpleAdd", "SimpleAdd.asm"))
+	asmFile, _ := os.ReadFile(filepath.Join("fixtures", "SimpleAdd", "SimpleAdd.asm"))
+	expected := bytes.NewBuffer(asmFile)
+	err := convert(in, got)
+	if err != nil {
+		t.Errorf("ERROR: %#v", err)
+	} else if got.String() != expected.String() {
+		t.Errorf("FAILED: expected %#v, got %#v", expected.String(), got.String())
+	}
+}
+
+func TestConvertStackTest(t *testing.T) {
+	vmFile, _ := os.ReadFile(filepath.Join("fixtures", "StackTest", "StackTest.vm"))
+	in := bytes.NewBuffer(vmFile)
+	got := bytes.NewBuffer([]byte{})
+	asmFile, _ := os.ReadFile(filepath.Join("fixtures", "StackTest", "StackTest.asm"))
 	expected := bytes.NewBuffer(asmFile)
 	err := convert(in, got)
 	if err != nil {
