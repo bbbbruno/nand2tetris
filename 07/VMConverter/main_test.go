4be6d23a288932079ff23e5826b33697b9daa720
bbbbruno <bbbbruno69@gmail.com>
2022-01-29 22:33:49 +0900

    fixturesにPointerTestを追加

--- 07/VMConverter/main_test.go
+++ 07/VMConverter/main_test.go
@@ -35,11 +35,25 @@ func TestConvertStackTest(t *testing.T) {
 	}
 }
 
-func TestConvertBasic(t *testing.T) {
+func TestConvertBasicTest(t *testing.T) {
 	vmFile, _ := os.ReadFile(filepath.Join("fixtures", "BasicTest", "BasicTest.vm"))
 	in := bytes.NewBuffer(vmFile)
 	got := bytes.NewBuffer([]byte{})
 	asmFile, _ := os.ReadFile(filepath.Join("fixtures", "BasicTest", "BasicTest.asm"))
+	expected := bytes.NewBuffer(asmFile)
+	err := convert(in, got)
+	if err != nil {
+		t.Errorf("ERROR: %#v", err)
+	} else if got.String() != expected.String() {
+		t.Errorf("FAILED: expected %#v, got %#v", expected.String(), got.String())
+	}
+}
+
+func TestConvertPointerTest(t *testing.T) {
+	vmFile, _ := os.ReadFile(filepath.Join("fixtures", "PointerTest", "PointerTest.vm"))
+	in := bytes.NewBuffer(vmFile)
+	got := bytes.NewBuffer([]byte{})
+	asmFile, _ := os.ReadFile(filepath.Join("fixtures", "PointerTest", "PointerTest.asm"))
 	expected := bytes.NewBuffer(asmFile)
 	err := convert(in, got)
 	if err != nil {
