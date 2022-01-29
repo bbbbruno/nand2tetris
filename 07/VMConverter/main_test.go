282b2a49fd07a1345609893b77bae18b1a1d9fd9
bbbbruno <bbbbruno69@gmail.com>
2022-01-29 13:56:45 +0900

    fixtureを追加して、そのテストを記述

--- 07/VMConverter/main_test.go
+++ 07/VMConverter/main_test.go
@@ -1,8 +1,22 @@
 package main
 
 import (
+	"bytes"
+	"os"
+	"path/filepath"
 	"testing"
 )
 
-func TestConvert(t *testing.T) {
+func TestConvertSimpleAdd(t *testing.T) {
+	vmFile, _ := os.ReadFile(filepath.Join("tests", "SimpleAdd", "SimpleAdd.vm"))
+	in := bytes.NewBuffer(vmFile)
+	got := bytes.NewBuffer([]byte{})
+	asmFile, _ := os.ReadFile(filepath.Join("tests", "SimpleAdd", "SimpleAdd.asm"))
+	expected := bytes.NewBuffer(asmFile)
+	err := convert(in, got)
+	if err != nil {
+		t.Errorf("ERROR: %#v", err)
+	} else if got.String() != expected.String() {
+		t.Errorf("FAILED: expected %#v, got %#v", expected.String(), got.String())
+	}
 }
