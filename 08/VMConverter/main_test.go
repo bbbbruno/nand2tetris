8542b7b7a885fc2ffe0f52e205011511ace1f73e
bbbbruno <bbbbruno69@gmail.com>
2022-01-31 15:46:53 +0900

    fixturesにBasicLoopとFibonacciSeriesを追加

--- 08/VMConverter/main_test.go
+++ 08/VMConverter/main_test.go
@@ -81,3 +81,33 @@ func TestConvertStaticTest(t *testing.T) {
 		t.Errorf("FAILED: expected %#v, got %#v", expected.String(), got.String())
 	}
 }
+
+func TestConvertBasicLoop(t *testing.T) {
+	filename := "BasicLoop"
+	vmFile, _ := os.ReadFile(filepath.Join("fixtures", filename, filename+".vm"))
+	in := bytes.NewBuffer(vmFile)
+	got := bytes.NewBuffer([]byte{})
+	asmFile, _ := os.ReadFile(filepath.Join("fixtures", filename, filename+".asm"))
+	expected := bytes.NewBuffer(asmFile)
+	err := convert(in, got, filename)
+	if err != nil {
+		t.Errorf("ERROR: %#v", err)
+	} else if got.String() != expected.String() {
+		t.Errorf("FAILED: expected %#v, got %#v", expected.String(), got.String())
+	}
+}
+
+func TestConvertFibonacciSeries(t *testing.T) {
+	filename := "FibonacciSeries"
+	vmFile, _ := os.ReadFile(filepath.Join("fixtures", filename, filename+".vm"))
+	in := bytes.NewBuffer(vmFile)
+	got := bytes.NewBuffer([]byte{})
+	asmFile, _ := os.ReadFile(filepath.Join("fixtures", filename, filename+".asm"))
+	expected := bytes.NewBuffer(asmFile)
+	err := convert(in, got, filename)
+	if err != nil {
+		t.Errorf("ERROR: %#v", err)
+	} else if got.String() != expected.String() {
+		t.Errorf("FAILED: expected %#v, got %#v", expected.String(), got.String())
+	}
+}
